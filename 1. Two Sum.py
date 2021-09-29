0098c2611440d687d7a414584acb620fde1d051e
Joyce_Tsou <84448972+c820306@users.noreply.github.com>
2021-09-30 00:07:25 +0800

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,3 +1,15 @@
+"""
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+"""
+
+# method 1
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -9,4 +21,22 @@ class Solution(object):
             for j in range(i+1, len(nums)):
                 if nums[i]+nums[j] == target:
                     return [i,j]
+                
+
+# method2
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        j = 0
+        k = 1
+        while nums[j] + nums[k] != target:
+            k += 1
+            if k == len(nums):
+                j += 1
+                k = j+1
+        return [j,k]
                 
